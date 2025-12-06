5f1624134058154aaa065e76dfde1708df268c74
Fancy Encinas <77sthy.fa@gmail.com>
2025-12-05 20:05:18 -0600

    feat: Ejercicio1 resuelto

---
+++ Ejercicio1/main.js
@@ -0,0 +1,15 @@
+/*Ejercicio #1 ( sugerencia join() )
+Copy the code below into your workspace:
+*/
+
+let arr = ["This", "is", "a", "sentence."];
+const printOutString = () => {
+  return arr.join(" ");
+};
+
+console.log(printOutString());
+//Complete the function to print out the string: This is a sentence.
+
+const printOutStringSimple = () => arr.join(" - ");
+
+console.log(printOutStringSimple());
